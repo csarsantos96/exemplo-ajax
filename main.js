59f985b2deef6be8cb1c79dd635dfe2c3d1e1bcc
Cesar Santos <csar.santos18@gmail.com>
2024-11-29 19:15:26 -0500

    ajax

--- main.js
+++ main.js
@@ -11,45 +11,45 @@
     })
 }) */
 
-$(document).ready(function(){
-    $('#cep').mask('00000-000');
-
-    $('#btn-buscar-cep').click(function(){
-        const cep =$('#cep').val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json/resultado`;
-        const botao = $(this);
-        $(botao).find('i').addClass('d-none');
-        $(botao).find('span').removeClass('d-none');
-        
-
-        /* $.ajax(endpoint).done(function(resposta){
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.estado;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco);
-
-            setTimeout(function(){
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 2000);
-        }) */
-        fetch(endpoint).then(function(resposta){
-            return resposta.json()
+    $(document).ready(function(){
+        $('#cep').mask('00000-000');
+    
+        $('#btn-buscar-cep').click(function(){
+            const cep =$('#cep').val();
+            const endpoint = `https://viacep.com.br/ws/${cep}/json/resultado`;
+            const botao = $(this);
+            $(botao).find('i').addClass('d-none');
+            $(botao).find('span').removeClass('d-none');
+            
+    
+            /* $.ajax(endpoint).done(function(resposta){
+                const logradouro = resposta.logradouro;
+                const bairro = resposta.bairro;
+                const cidade = resposta.localidade;
+                const estado = resposta.estado;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+                $('#endereco').val(endereco);
+    
+                setTimeout(function(){
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                }, 2000);
+            }) */
+            fetch(endpoint).then(function(resposta){
+                return resposta.json()
+            })
+            .then(function(json){
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.estado;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+                $('#endereco').val(endereco);
+    
+                setTimeout(function(){
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                }, 2000);
+            })
+            })
         })
-        .then(function(json){
-            const logradouro = json.logradouro;
-            const bairro = json.bairro;
-            const cidade = json.localidade;
-            const estado = json.estado;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco);
-
-            setTimeout(function(){
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 2000);
-        })
-        })
-    })
