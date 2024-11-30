5f3e182118775fcd6c50822b244c8eb133cd5a6c
Cesar Santos <csar.santos18@gmail.com>
2024-11-29 20:19:56 -0500

    Exceções

--- main.js
+++ main.js
@@ -16,12 +16,11 @@
     
         $('#btn-buscar-cep').click(function(){
             const cep =$('#cep').val();
-            const endpoint = `https://viacep.com.br/ws/${cep}/json/resultado`;
+            const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
             const botao = $(this);
             $(botao).find('i').addClass('d-none');
             $(botao).find('span').removeClass('d-none');
             
-    
             /* $.ajax(endpoint).done(function(resposta){
                 const logradouro = resposta.logradouro;
                 const bairro = resposta.bairro;
@@ -35,7 +34,8 @@
                     $(botao).find('span').addClass('d-none');
                 }, 2000);
             }) */
-            fetch(endpoint).then(function(resposta){
+            fetch(endpoint)
+            .then(function(resposta){
                 return resposta.json()
             })
             .then(function(json){
@@ -45,11 +45,22 @@
                 const estado = json.estado;
                 const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
                 $('#endereco').val(endereco);
-    
+            })
+            .catch(function(erro){
+                alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+            })
+            .finally(function(){
                 setTimeout(function(){
                     $(botao).find('i').removeClass('d-none');
                     $(botao).find('span').addClass('d-none');
-                }, 2000);
+                    },1000);
+                })
             })
+            $('#formulario-pedido').submit(function(evento){
+                evento.preventDefault();
+                
+                if ($('#nome').val().length == 0){
+                    throw new Error('Digite o nome');
+                }
             })
         })
